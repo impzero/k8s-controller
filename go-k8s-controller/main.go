b6ac9f7a9e265a4916a6060eb198942ba833a2f5
Lyubo Lyubchev <lyubo_2317@abv.bg>
2024-02-10 21:58:37 +0100

    wip: add Kubernetes informer for Zephy resource

--- go-k8s-controller/main.go
+++ go-k8s-controller/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"path/filepath"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
@@ -32,9 +37,19 @@ func main() {
 		panic(err)
 	}
 
-	_ = schema.GroupVersionResource{
+	zephySchema := schema.GroupVersionResource{
 		Group:    "myk8s.io",
 		Version:  "v1",
 		Resource: "zephy",
 	}
+
+	informer := cache.NewSharedIndexInformer(&cache.ListWatch{
+		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
+			return c.Resource(zephySchema).List(context.TODO(), options)
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			return c.Resource(zephySchema).Watch(context.TODO(), options)
+		},
+		DisableChunking: false,
+	}, &metav1.Pod{}, 0, cache.Indexers{})
 }
